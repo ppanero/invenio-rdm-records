54a39e682f901ad3e925a05175ec41e19d1d78e8
Maximilian Moser <maximilian.moser@tuwien.ac.at>
2020-11-24 12:13:55 +0100

    tests: fix failing EDTFListDumperExt test
    * the optional metadata.contributors field was removed from the
      minimal_record fixture
    * use the required metadata.creators field instead of
      metadata.contributors

--- tests/records/test_dumpers.py
+++ tests/records/test_dumpers.py
@@ -142,7 +142,7 @@ def test_eslistdumper_with_edtfext_parse_error(app, db, minimal_record):
     """Test edft extension implementation."""
     dumper = ElasticsearchDumper(
         extensions=[
-            EDTFListDumperExt("metadata.contributors", "name"),
+            EDTFListDumperExt("metadata.creators", "name"),
         ]
     )
 
@@ -152,10 +152,10 @@ def test_eslistdumper_with_edtfext_parse_error(app, db, minimal_record):
 
     # Dump it
     dump = record.dumps(dumper=dumper)
-    assert "name_range" not in dump["metadata"]["contributors"][0]
-    assert "name" in dump["metadata"]["contributors"][0]
+    assert "name_range" not in dump["metadata"]["creators"][0]
+    assert "name" in dump["metadata"]["creators"][0]
 
     # Load it
     new_record = BibliographicRecord.loads(dump, loader=dumper)
-    assert "name_range" not in new_record["metadata"]["contributors"][0]
-    assert "name" in new_record["metadata"]["contributors"][0]
+    assert "name_range" not in new_record["metadata"]["creators"][0]
+    assert "name" in new_record["metadata"]["creators"][0]
